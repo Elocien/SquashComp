10837300fe05aef1dff500379e721a639d759235
Mohammad Usman <mohammad.usman@peergroup.com>
2021-11-21 12:06:46 +0100

    Added login/logout

--- ClubLeagueSite/urls.py
+++ ClubLeagueSite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('league/', include('League.urls')),
     path('admin/', admin.site.urls),
+	path('league/', include('django.contrib.auth.urls')),
+	path('league/', include('League.urls')),
 ]
