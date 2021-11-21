5f99c2389917e499645dc9372e74b6f43ef0c9a2
Mohammad Usman <mohammad.usman@peergroup.com>
2021-11-21 23:12:07 +0100

    Configured base template. Added css styling for group view. Added page for entering score.

--- League/views.py
+++ League/views.py
@@ -7,3 +7,8 @@ def index(request):
 	template = loader.get_template('League/index.html')
 	context = {'data': "data",}
 	return HttpResponse(template.render(context, request))
+
+def score(request):
+	template = loader.get_template('League/match_score.html')
+	context = {'data': "data",}
+	return HttpResponse(template.render(context, request))
